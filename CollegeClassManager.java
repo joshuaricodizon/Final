788b669896736397066cf2e3c825b67691be8dd6
Joshua Dizon <dizonjoshua16@gmail.com>
2023-12-11 10:22:32 -0800

    Add more GUI, added switch for program options

--- CollegeClassManager.java
+++ CollegeClassManager.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
@@ -87,55 +88,70 @@ public class CollegeClassManager {
         engGraph.addEdge("ENG101", "ENG202");
         engGraph.addEdge("ENG202", "ENG303");
         
-        // Program header
-        System.out.println("*****************************************");
-        System.out.println("*     College Course Road Map Helper    *");
-        System.out.println("*****************************************");
-        
-
-
-        // Get user input for CIS classes
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the current CIS class: ");
-        String currentCISClass = scanner.nextLine();
-        List<String> cisRecommendations = classManager.recommendNextClass(currentCISClass, CIS);
 
-        // Get user input for MAT classes
-        System.out.print("Enter the current MAT class: ");
-        String currentMATClass = scanner.nextLine();
-        List<String> matRecommendations = classManager.recommendNextClass(currentMATClass, MAT);
+        while (true) {
+            // Display menu options with flipped order
+        	displayHeader("main");
+            System.out.println("\nMenu Options:");
+            System.out.println("0 - View all classes");
+            System.out.println("1 - View recommended ENG class");
+            System.out.println("2 - View recommended CIS class");
+            System.out.println("3 - View recommended MAT class");
+            System.out.println("00 - Exit program");
 
-        // Get user input for ENG classes
-        System.out.print("Enter the current ENG class: ");
-        String currentENGClass = scanner.nextLine();
-        List<String> engRecommendations = classManager.recommendNextClass(currentENGClass, ENG);
-        
-        
-     // Program header
-        System.out.println("*****************************************");
-        System.out.println("*          Recommended Courses           *");
-        System.out.println("*****************************************");
-
-        // Print recommendations
-        System.out.println("\nRecommended CIS Classes to take after " + currentCISClass + ": " + cisRecommendations);
-        System.out.println("Recommended MAT Classes to take after " + currentMATClass + ": " + matRecommendations);
-        System.out.println("Recommended ENG Classes to take after " + currentENGClass + ": " + engRecommendations);
-        
-        
-        // Program header
-        System.out.println("*****************************************");
-        System.out.println("*          All Courses And Pre-reqs       *");
-        System.out.println("*****************************************");
-        
-        
-        System.out.println("\nAll CIS Classes with pre-reqs:");
-        displayAllClasses(cisGraph);
-        
-        System.out.println("\nAll MAT Classes with pre-reqs:");
-        displayAllClasses(matGraph);
-        
-        System.out.println("\nAll ENG Classes with pre-reqs:");
-        displayAllClasses(engGraph);
+            // Get user input for menu option
+            System.out.print("Enter your choice: ");
+            String choice = scanner.nextLine();
+            
+            
+            // Show header
+            displayHeader("main");
+            switch (choice) {
+            case "0":
+                // Display all classes with dependencies
+            	System.out.println("*************************");
+            	System.out.println("ENG Course Progression: ");
+            	displayAllClasses(engGraph);
+            	System.out.println("*************************");
+            	System.out.println("CIS Course Progression: ");
+                displayAllClasses(cisGraph);
+                System.out.println("*************************");
+                System.out.println("MAT Course Progression: ");
+                displayAllClasses(matGraph);
+                System.out.println("*************************");
+                break;
+                case "1":
+                    // Get user input for ENG classes
+                    System.out.print("Enter your current ENG class: ");
+                    String currentENGClass = scanner.nextLine();
+                    List<String> engRecommendations = classManager.recommendNextClass(currentENGClass, ENG);
+                    displayHeader("recommend");
+                    System.out.println("Recommended ENG Classes to take after " + currentENGClass + ": " + engRecommendations);
+                    break;
+                case "2":
+                    // Get user input for CIS classes
+                    System.out.print("Enter your current CIS class: ");
+                    String currentCISClass = scanner.nextLine();
+                    List<String> cisRecommendations = classManager.recommendNextClass(currentCISClass, CIS);
+                    displayHeader("recommend");
+                    System.out.println("Recommended CIS Classes to take after " + currentCISClass + ": " + cisRecommendations);
+                    break;
+                case "3":
+                    // Get user input for MAT classes
+                    System.out.print("Enter your current MAT class: ");
+                    String currentMATClass = scanner.nextLine();
+                    List<String> matRecommendations = classManager.recommendNextClass(currentMATClass, MAT);
+                    displayHeader("recommend");
+                    System.out.println("Recommended MAT Classes to take after " + currentMATClass + ": " + matRecommendations);
+                    break;
+                case "00":
+                    System.out.println("Exiting program. Goodbye!");
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice. Please enter a valid option.");
+            }
+        }
  
     }
     
@@ -162,4 +178,23 @@ public class CollegeClassManager {
             System.out.println();
         }
     }
+    
+    private static void displayHeader(String headerType) {
+    	
+    	if (headerType == "main") {
+    		System.out.println("*****************************************");
+            System.out.println("*     College Course Road Map Helper    *");
+            System.out.println("*****************************************");
+    		
+    	}else if(headerType == "exit") {
+    		System.out.println("*****************************************");
+            System.out.println("*     Closing Course Road Map Helper    *");
+            System.out.println("*****************************************");
+    		
+    	}else {
+    		 System.out.println("System error.");
+    	}
+    	
+    }
+
 }
